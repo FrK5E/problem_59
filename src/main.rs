35cb89e8270f0fa9b6ca7f4438ca9b7867ae02bf
FrK5E <FrK5Ewzb4XbBN1aWlGUZ@proton.me>
2023-09-11 21:28:10 +0200

    some progress

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 use std::fs;
 
+
+fn print_text( data: & Vec<u8> ) {
+    let mut text = String::from("");
+    for k in data { 
+        text.push( *k as char )
+    }
+    println!("{text}"); 
+}
+
 fn main() {
 
-    let contents = fs::read_to_string("0059_cipher.txt")
+    let contents = fs::read_to_string("0059_plain_test_text.txt")
         .expect("Should have been able to read the file");
 
-    let letters = contents.split(",");
-    let mut text = Vec::new(); 
+    let letters = contents.trim().split(",");
+    let mut data = Vec::new(); 
     for lt in letters {
-        let num = lt.parse::<u8>().unwrap(); 
-        text.push(num);
-        let temp: i32 = num.into();
+        let result = lt.parse::<u8>().unwrap();
+        data.push(result);
+        let temp: i32 = result.into();
         println!( "{temp}");
     }
 
-    
+    print_text(&data);
 
     //println!("The text: {contents}")
 }
