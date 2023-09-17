f70628e2d728fb962490cec199363a762b084991
FrK5E <FrK5Ewzb4XbBN1aWlGUZ@proton.me>
2023-09-17 21:55:55 +0200

    This seems find the decoding sequence.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::{fs, ops::BitXor};
 
-fn print_text(data: &Vec<u8>) {
-    let mut text = String::from("");
+fn convert_to_string(data: &Vec<u8>, result: &mut String) {
+    result.clear();
     for k in data {
-        text.push(*k as char)
+        result.push(*k as char)
     }
-    println!("{text}");
 }
 
 fn apply_key(data: &mut Vec<u8>, key: &Vec<u8>) {
@@ -14,38 +13,61 @@ fn apply_key(data: &mut Vec<u8>, key: &Vec<u8>) {
     }
 }
 
+fn detect_english(data: &Vec<u8>) -> bool {
+    let mut string1 = String::with_capacity(data.len());
+    convert_to_string(&data, &mut string1);
 
+    let number_of_spaces = string1.matches(" ").count();
+    if (data.len() as f64)/(number_of_spaces as f64) > 8.0 { 
+        return false;
+    }
+
+
+    let frequent_words = [
+        "the", "of", "and", "a", "to", "in", "is", "you", "that", "it",
+    ];
+    
+    
+    let mut found = 0i32;  
+    for word in frequent_words {
+        if string1.contains(word) {
+            found += 1;
+        }
+    }
+
+    if ( found >= 8 ) { 
+        println!("Found {found} words.\n {string1}");
+        return true;
+    }
+
+    return false;
+}
 
 fn main() {
-    let frequent_words = [ "the", "of", "and", "a", "to", "in", 
-    "is", "you", "that", "it"  ];
-
-
-    let contents = fs::read_to_string("0059_plain_test_text.txt")
-        .expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string("0059_cipher.txt").expect("Should have been able to read the file");
 
     let letters = contents.trim().split(",");
     let mut data = Vec::new();
     for lt in letters {
         let result = lt.parse::<u8>().unwrap();
         data.push(result);
-        let temp: i32 = result.into();
-        println!("{temp}");
     }
 
-    print_text(&data);
-
-    // 141 = 'a' 
-    // 172 = 'z' 
-    let key = vec![120u8, 121u8, 123u8];
-
-    apply_key(&mut data, &key);
-
-    print_text(&data);
-
-    apply_key(&mut data, &key);
-
-    print_text(&data);
-
-    //println!("The text: {contents}")
+    let mut counter = 1u32;
+    for first_char in 97u8..122u8+1u8 {
+        for second_char in 97u8..122u8+1u8 {
+            for third_char in 97u8..122u8+1u8 {
+                let key = vec![first_char, second_char, third_char];
+                apply_key(&mut data, &key);
+                if detect_english(&data) {
+                    println!("Heureka ");
+                    return;
+                }
+                counter +=1;
+                println!( "{counter}" ); 
+                apply_key(&mut data, &key);
+            }
+        }
+    }
 }
